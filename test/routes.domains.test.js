76f1c07e6b5d30eaefc0b85fd3353cfc31cb4a93
AchimG <project_design@web.de>
2019-12-09 13:08:55 +0100

    Add new test case

--- test/routes.domains.test.js
+++ test/routes.domains.test.js
@@ -37,4 +37,23 @@ describe('Routes: domains', () => {
       expect(res.body.data).toEqual(domain);
     });
   });
+
+  describe('POST /domain', () => {
+    test('Should return single post after insert', async () => {
+      const newDom = { domain: newdomain.com };
+      const res = await chai.request(server).post('/domain').send(newDom);
+
+      expect(res.status).toEqual(200);
+      expect(res.body.data).toBeDefined();
+      expect(res.body.data).toEqual(newDom);
+    });
+
+    test('Should return error status message, when body invalid', async () => {
+      const res = await chai.request(server).post('/domain').send({ domain: '' });
+
+      expect(res.status).toEqual(200);
+      expect(res.body.data).toBeDefined();
+    });
+  });
+
 });
