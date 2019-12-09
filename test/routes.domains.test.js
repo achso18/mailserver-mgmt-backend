b229518731dfa3e8b441f109455838568278e16d
AchimG <project_design@web.de>
2019-12-09 12:37:37 +0100

    add new test case

--- test/routes.domains.test.js
+++ test/routes.domains.test.js
@@ -20,8 +20,21 @@ describe('Routes: domains', () => {
   describe('GET /domains', ()=> {
     test('Should return array of domains', async () => {
       const res = await chai.request(server).get('/domains');
+      const domains = [ { id: 1, domain: 'example.com' },
+                        { id: 2, domain: 'my.dom' }
+                      ];
+      expect(res.status).toEqual(200);
+      expect(res.body.data).toEqual(domains);
+    });
+  });
+
+  describe('GET /domain/:id', () => {
+    test('Should return a single domain', async () => {
+      const domain = { id: 1, domain: 'example.com'};
+      const res = await chai.request(server).get('domain/1');
       expect(res.status).toEqual(200);
       expect(res.body.data).toBeDefined();
+      expect(res.body.data).toEqual(domain);
     });
   });
 });
