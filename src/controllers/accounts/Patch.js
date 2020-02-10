fd2887a64b38f9aac220c6951d2a8d32f9eddb0a
AchimG <project_design@web.de>
2020-02-10 09:27:46 +0100

    implement patch function

--- src/controllers/accounts/Patch.js
+++ src/controllers/accounts/Patch.js
@@ -1,3 +1,22 @@
 const Accounts = require('../../models/Accounts');
 
-module.exports = () => {};
+module.exports = async ctx => {
+  try {
+    const { id } = ctx.params;
+
+    const affectedRows = await Accounts.query().patch(ctx.request.body).where({ id });
+    if (!affectedRows) ctx.throw(409, { data: { message: 'Error updating domain' } });
+
+    ctx.status = 200;
+    ctx.body = {
+      data: ctx.request.body
+    };
+
+  } catch (e) {
+    const message = 'Internal server error';
+    ctx.status = e.statusCode || 500;
+    ctx.body = {
+      error: e.data || { message }
+    };
+  }
+};
